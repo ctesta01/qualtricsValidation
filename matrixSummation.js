fe13ecf56eb743ad0de265cf5636a739ef708497
Christian Testa <christian.testa@jomi.com>
2015-08-25 19:44:20 -0400

    Rewrote matrixSummation.js

--- matrixSummation.js
+++ matrixSummation.js
@@ -5,21 +5,23 @@ function sumMatrix(selectionSet, outputCell) {
     // When you call sumMatrix, this is the summing part.
     function sumSet(set, output) {
         var setTotal = 0;
-        for (var j=0; j < (set.length - 1); j++) {
+        for (var j=0; j < (set.length - 2); j++) {
             var setInputValue = parseInt(set[j].down().value);
             if (isNaN(setInputValue)) setInputValue = 0;
             setTotal = setTotal + setInputValue;
         }
-        output = setTotal;
+        output.value = setTotal;
     }
     
     // This is the manipulation of the frontend of the matrix.
     // This makes it such that where the sum goes is readonly
     // And ensures that when people do stuff, the matrix updates.
-    setSize = selectionSet.length;
+    setSize = selectionSet.length - 1;
     outputCell.setAttribute("readonly", "readonly");
     for (var i=0; i < setSize; i++) {
-       selectionSet[i].down().observe("keyup", sumSet(selectionSet, outputCell));
+       selectionSet[i].down().observe("keyup", function(){
+       sumSet(selectionSet, outputCell)
+       });
     } 
 }
 
