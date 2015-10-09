2b5921213e95ab868c732249f7a1a13f3f351f6c
Christian Testa <ctesta01@users.noreply.github.com>
2015-10-09 12:24:28 -0400

    Update oireQualtricsLibrary.js

--- oireQualtricsLibrary.js
+++ oireQualtricsLibrary.js
@@ -132,17 +132,19 @@ function qualtricsLess(lessCell, greatCell) {
 }
 
 function qualtricsEqual(array, watchCells) {
-    // 
+    // default watchCells to array
     if (watchCells === undefined) {
         watchCells = array;
     }
 
+    // wrap mathEqual() with watchSet so that mathEqual() is run each time the watchCells are edited
     watchSet(watchCells, function(){
         mathEqual(array);
     });
 }
 
 function qualtricsSum(array, output) {
+    // wrap mathSum() with watchSet so that mathSum() is run each time its array is updated
     watchSet(array, function(){
         mathSum(array, output);
     });
@@ -150,9 +152,11 @@ function qualtricsSum(array, output) {
 }
 
 function hideBox(cells) {
+    // if single cell, set visibility to hidden
     if (isCell(cells)) {
         cells.down().setAttribute("style", "visibility:hidden;");
     }
+    // if array of cells, set each cell to hidden
     if (isCellArray(cells)) {
         for (var i=0; i<cells.length; i++) {
             cells[i].down().setAttribute("style", "visibility: hidden;");
@@ -161,36 +165,58 @@ function hideBox(cells) {
 }
 
 function cell(string, qid) {
+    // regex to match cell column (r1), row (r2)
     var r1 = /^[A-Z]/;
     var r2 = /\d{1,3}/;
     var column = r1.exec(string);
     var row = r2.exec(string.replace(r1, ""));
+    
+    // currently defaulting to QID1, should default to this.Question if possible...
     if (qid === undefined) {
+        // TODO fix default QuestionId
         //qid = this.QuestionId;
         qid = "QID1";
     }
+    
+    // error on invalid column
     if (column === null) {
         alert(string.concat(": Cell misformatted. Column is not single uppercase letter"));
     }
+    
+    // error on invalid row
     if (row === null) {
         alert(string.concat(": Cell misformatted. Row is not number between 1 and 3 digits"));
     }
+    
+    // convert column from letter value to integer value
     var qColumn = column[0].charCodeAt(0) - 61;
+    
+    // adjust row value
     var qRow = row - 1;
+    
+    // return matching cell
     return ($(qid).select("td.c".concat(qColumn))[qRow]);
 }
 
 function isCell(cell) {
+    // basically testing if the string matches <td></td> tags
     var re = /<td[^>]*>((?:.|\r?\n)*?)<\/td>/g;
     return re.test(cell.outerHTML);
 }
 
 function isCellArray(array) {
+    // regex for matching <td></td> tags
     var re = /<td[^>]*>((?:.|\r?\n)*?)<\/td>/;
+    
+    // default to true
     var arrayStatus = true;
+    
+    // check that the type of array is actually Array
     if (array.constructor !== Array) {
         arrayStatus = false;
     }
+    
+    // loop through and check each element in the array against regex (re)
     for (var i=0; i<array.length; i++) {
         if (re.test(array[i].outerHTML) === false) {
             arrayStatus = false;
@@ -200,19 +226,23 @@ function isCellArray(array) {
 }
 
 function cellRange(startCell, endCell, qid) {
+    // regex to match column and row
     var r1 = /^[A-Z]/;
     var r2 = /[0-9]{1,3}$/;
 
+    // default question ID should be this.QuestionId
     if (qid === undefined) {
         qid = this.QuestionId;
     }
 
 
+    // convert from cell names to values
     var startCellColumn = r1.exec(startCell)[0].charCodeAt(0) - 61;
     var endCellColumn = r1.exec(endCell)[0].charCodeAt(0) - 61;
     var startCellRow = parseInt(r2.exec(startCell)[0], 10);
     var endCellRow = parseInt(r2.exec(endCell)[0], 10);
 
+    // instantiate tempRange, so that we can push things onto 
     var tempRange = [];
     for (var q=startCellColumn; q<=endCellColumn; q++) {
         for (var r=startCellRow; r<=endCellRow; r++) {
@@ -228,14 +258,27 @@ function cellRange(startCell, endCell, qid) {
     return outputRange;
 }
 
+
 function mathCalc(origString, output, qid) {
+    // make a copy of the original string
     var string = origString;
+    
+    // regex to match cells
     var cellMatch = /^[A-Za-z0-9\s]{2,4}/;
+    
+    // regex to match valid operators +, -, *, /, (, )
     var operatorMatch = /^[\+\-\/\*()\s]/;
+    
+    // regex to match valid user input for the arithmetic operation
     var validStringMatch = /[A-Za-z0-9\+\-\/\*\s]{2,50}/g;
+    
+    // operation is a stack we will push the final operation to be run onto
     var operation = [];
+    
+    // cells is a stack we will push cells that are relevant to the operation onto
     var cells = [];
 
+    // default qid is this.QuestionId
     if (qid === undefined) {
         qid = this.QuestionId;
     }
@@ -245,22 +288,35 @@ function mathCalc(origString, output, qid) {
         alert(string.concat(" is a non-valid arithmetic expression"));
     }
 
+    // regMatchCellString() is a local function, that pushes each cell and operator onto the operation array
     function regMatchCellString(str) {
+        // check for each of these cases
         switch (true) {
+          // if the beginning of the string is a cell, push the cell's value onto the operation stack
           case cellMatch.test(str):
+              // if the cell is empty, push 0
               if (cell(cellMatch.exec(string)[0].trim(), qid).down().value === "") {
                   operation.push("0");
               }
+              // otherwise push its value
               else {
                   operation.push(cell(cellMatch.exec(string)[0].trim(), qid).down().value);
               }
+              // additionally push the cell itself onto the cell stack
               cells.push(cell(cellMatch.exec(string)[0].trim(), qid));
+              
+              // remove the cell from the string, so that we can move on to the next cell / operator
               string = string.replace(cellMatch, "");
             break;
+            
+          // if the beginning of the string is an operator, 
+          // push the operator onto the operation stack and remove it from the string 
           case operatorMatch.test(str):
                 operation.push(operatorMatch.exec(string)[0]);
                 string = string.replace(operatorMatch, "");
             break;
+            
+          // if the string's beginning is neither a valid cell nor an operator, error!
           default:
                 console.log(string);
                 console.log("String didn't match as a cell or operator");
@@ -268,22 +324,41 @@ function mathCalc(origString, output, qid) {
         }
     }
 
+    // run regMatchCellString() on string until it finishes
+    // each part of the string will be parsed into the operation stack
+    // and each of the cells will be saved in the cells stack
     while (string !== "") {
         regMatchCellString(string);
     }
     
     setReadOnly(output);
+    
+    // evaluate the operation stack as one long operation and store it to outputValue
     outputValue = eval(operation.join(""));
+    
+    // if outputValue isn't finite, then output 0 instead
+    // nobody really likes NaN
+    // 1/0 == 0 because of this 
     if (isFinite(outputValue) === false) {
         outputValue = 0;
     }
+    
+    // return an array of two elements
+    // the first being the output value
+    // the second being the array of relevant cells
     return [outputValue, cells];
 }
 
 function qualtricsMath(string, output, qid, watchCells) {
+    
+    // set default cells to be watched as the cells-stack from mathCalc()'s output
     if (watchCells === undefined) {
         watchCells = mathCalc(string,output,qid)[1];
     }
+    
+    // here we wrap a nameless function that updates the output cell's value 
+    // with the outputValue from mathCalc()'s return value with watchSet so that whenever watchCells
+    // are updated, the mathCalc() runs again and the output cell's value gets updated again
     watchSet(watchCells, function(){
         output.down().value = mathCalc(string,output,qid)[0];
     });
